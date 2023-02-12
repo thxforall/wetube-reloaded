3249333c7ea3652483365919ccdac947191b614c
thxforall <brlduf@naver.com>
2023-02-12 16:53:40 +0900

    pug, render

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,8 @@
-export const trending = (req, res) => res.send("Home Page Trending Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send(`Edit Video #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => {
   return res.send(`Delete Video #${req.params.id}`);
 };
