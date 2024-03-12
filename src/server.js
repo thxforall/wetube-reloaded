0c542618d6f48020c99a59f8ea16cfc139b8283c
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-12 22:32:49 +0900

    First Step Wetube Clone : NodeJS

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', function (req, res) {
+  return res.send('Hi');
+});
+
+app.get('/login', function (req, res) {
+  return res.send('Login Here');
+});
+
+const handleListening = () => {
+  console.log(`✅ Server listening on port http://localhost:${PORT} 😀`);
+};
+
+app.listen(PORT, handleListening);
