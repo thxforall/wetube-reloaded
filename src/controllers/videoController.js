949c8a295198f64ddf53b7c0ed5c8c6930a236fb
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-18 22:43:44 +0900

    Pug and Template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,18 @@
-export const trending = (req, res) => res.send('Home Page Videos');
+export const trending = (req, res) => {
+  res.render('home', { pageTitle: 'Home' });
+};
 export const see = (req, res) => {
-  console.log(req.params);
-  res.send(`Watch Video #${req.params.id}`);
+  res.render('watch', { pageTitle: 'Watch' });
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  res.send(`Edit Video #${req.params.id}`);
+  res.render('edit', { pageTitle: 'Edit' });
 };
-export const search = (req, res) => res.send('Search');
-export const upload = (req, res) => res.send('Upload');
+export const search = (req, res) => {
+  res.send('Search');
+};
+export const upload = (req, res) => {
+  res.send('Upload');
+};
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send(`Delete Video #${req.params.id}`);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+
 import globalRouter from './routers/globalRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
@@ -8,10 +9,15 @@ const PORT = 3000;
 
 const app = express();
 const logger = morgan('dev');
-app.use(logger);
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
+app.use(logger);
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
 
-app.listen(PORT);
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
