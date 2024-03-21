422dd9a7981e16fa9db7ec6abd023b0b223725d1
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-22 01:44:50 +0900

    POST db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,24 +1,67 @@
-const fakeUser = {
-  username: 'kiyori',
-  loggedIn: false,
-};
+let videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createAt: '2 minutes ago',
+    views: 1,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 5,
+    comments: 2,
+    createAt: '2 minutes ago',
+    views: 20,
+    id: 2,
+  },
+  {
+    title: 'Third Video',
+    rating: 5,
+    comments: 2,
+    createAt: '2 minutes ago',
+    views: 20,
+    id: 3,
+  },
+];
 
 export const trending = (req, res) => {
-  res.render('home', { pageTitle: 'Home', fakeUser });
+  res.render('home', { pageTitle: 'Home', videos });
 };
-export const see = (req, res) => {
-  res.render('watch', { pageTitle: 'Watch' });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
 };
-export const edit = (req, res) => {
-  res.render('edit', { pageTitle: 'Edit' });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('edit', { pageTitle: `Editing ${video.title}`, video });
 };
-export const search = (req, res) => {
-  res.send('Search');
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  console.log(req.body);
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
-export const upload = (req, res) => {
-  res.send('Upload');
+
+export const getUpload = (req, res) => {
+  return res.render('upload', { pageTitle: 'Upload Video' });
 };
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  res.send(`Delete Video #${req.params.id}`);
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: 'just now',
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect('/');
 };
