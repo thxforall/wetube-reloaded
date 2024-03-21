ea16780c17feb02206ed6d197ccfb140436c3b3b
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-21 22:43:32 +0900

    pug template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,10 @@
+const fakeUser = {
+  username: 'kiyori',
+  loggedIn: false,
+};
+
 export const trending = (req, res) => {
-  res.render('home', { pageTitle: 'Home' });
+  res.render('home', { pageTitle: 'Home', fakeUser });
 };
 export const see = (req, res) => {
   res.render('watch', { pageTitle: 'Watch' });
