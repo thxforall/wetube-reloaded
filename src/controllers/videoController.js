32e59c50c7cb0282f4c86b616e6dace5b026648d
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-28 05:28:29 +0900

    mongoose update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,20 +4,39 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render('home', { pageTitle: 'Home', videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render('watch', { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video Not Found' });
+  } else {
+    return res.render('watch', { pageTitle: video.title, video });
+  }
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render('edit', { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video Not Found' });
+  } else {
+    return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
+  }
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  console.log(req.body);
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video Not Found' });
+  } else {
+    await Video.findByIdAndUpdate(id, {
+      title,
+      description,
+      hashtags,
+    });
+    return res.redirect(`/videos/${id}`);
+  }
 };
 
 export const getUpload = (req, res) => {
